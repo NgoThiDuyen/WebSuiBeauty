d1857f8b61997f0ea7f47f3c3ca49fa5129317f6
HOANGDIEU_11\Nguyen Hoang Dieu <hoangdieu8023@gmail.com>
2023-05-15 09:35:24 +0700

    commit

---
+++ WebSuiBeauty/Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebSuiBeauty.Data;
+
+namespace WebSuiBeauty.Controllers
+{
+    public class AccountController : Controller
+    {
+        WebSuiBeautyDbContext db = new WebSuiBeautyDbContext();
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public ActionResult Login(FormCollection formColl)
+        //{
+        //    string usrName = formColl["UserName"];
+        //    string Pass = formColl["Password"];
+
+        //    if (ModelState.IsValid)
+        //    {
+        //        var cust = (from m in db.Customers
+        //                    where (m.UserName == usrName && m.Password == Pass)
+        //                    select m).SingleOrDefault();
+
+        //        if (cust != null)
+        //        {
+        //            TempShpData.UserID = cust.CustomerID;
+        //            Session["username"] = cust.UserName;
+        //            return RedirectToAction("Index", "Home");
+        //        }
+
+        //    }
+        //    return View();
+        //}
+    }
+}
---
+++ WebSuiBeauty/Data/Customer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+using WebSuiBeauty.Data.Abstract;
+
+namespace WebSuiBeauty.Data
+{
+    public class Customer : Auditable
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "First Name")]
+        public string First_Name { get; set; }
+        [Display(Name = "Last Name")]
+        public string Last_Name { get; set; }
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        [Display(Name = "Phone Number")]
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        [Display(Name = "Avatar")]
+        public string Avatar { get; set; }
+    }
+}
